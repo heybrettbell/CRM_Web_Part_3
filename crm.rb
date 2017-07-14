f5c9b68d83aa7c73f9cd687f6a859591ebc1423c
heybrettbell <brett@grassrootsonline.ca>
2017-07-14 14:38:36 -0400

    Assignment complete

--- crm.rb
+++ crm.rb
@@ -15,7 +15,7 @@ get '/contacts' do
 end
 
 get '/contacts/new' do
-  erb :forms
+  erb :new
 end
 
 get '/contacts/:id' do
@@ -43,6 +43,42 @@ get '/contacts/:id/edit' do
   @contact = Contact.find_by(id: params[:id].to_i)
   if @contact
     erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+
+get '/contacts/:id/delete' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :delete_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
   else
     raise Sinatra::NotFound
   end
