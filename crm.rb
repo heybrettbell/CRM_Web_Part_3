6e00bc8db0516d586776ca83b2ec41871e7959d0
heybrettbell <brett@grassrootsonline.ca>
2017-07-14 13:17:12 -0400

    Added new form page

--- crm.rb
+++ crm.rb
@@ -14,6 +14,10 @@ get '/contacts' do
   erb :contacts
 end
 
+get '/contacts/new' do
+  erb :forms
+end
+
 get '/contacts/:id' do
   # params[:id] contains the id from the URL
   @contact = Contact.find_by({id: params[:id].to_i})
@@ -23,6 +27,7 @@ get '/contacts/:id' do
    raise Sinatra::NotFound
  end
 end
+
 
 after do
   ActiveRecord::Base.connection.close
